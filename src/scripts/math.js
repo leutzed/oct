13446e23f4df8e1bb42fd6b9aab9f4db92b918db
Daniel Leutze <leutzed@gmail.com>
2023-10-19 18:18:26 +0000

    indentation fix

--- src/scripts/math.js
+++ src/scripts/math.js
@@ -1,79 +1,79 @@
 import { formatTimeWithoutMinutes, timeToDate } from "./utils.js";
 
 export function calculateAo5(arrayTimes) {
-    let arrayInDate = [];
+  let arrayInDate = [];
 
-    if (arrayTimes.length < 5) {
-        return "The array needs to have at least 5 records";
-    }
+  if (arrayTimes.length < 5) {
+    return "The array needs to have at least 5 records";
+  }
 
-    arrayTimes.forEach(function (unit) {
-        let formattedTime = formatTimeWithoutMinutes(unit);
-        let timeInDate = timeToDate(formattedTime)
-        arrayInDate.push(timeInDate)
-    });
+  arrayTimes.forEach(function (unit) {
+    let formattedTime = formatTimeWithoutMinutes(unit);
+    let timeInDate = timeToDate(formattedTime);
+    arrayInDate.push(timeInDate);
+  });
 
-    arrayInDate = arrayInDate.slice(-5);
-    arrayInDate.sort(function (a, b) {
-        return a - b;
-    });
-    
-    arrayInDate = arrayInDate.slice(1, -1);
-    var sum = arrayInDate.reduce(function (total, time) {
-        return total + time;
-    }, 0);
+  arrayInDate = arrayInDate.slice(-5);
+  arrayInDate.sort(function (a, b) {
+    return a - b;
+  });
 
-    var average = sum / arrayInDate.length;
+  arrayInDate = arrayInDate.slice(1, -1);
+  var sum = arrayInDate.reduce(function (total, time) {
+    return total + time;
+  }, 0);
 
-    return average;
+  var average = sum / arrayInDate.length;
+
+  return average;
 }
 
 export function calculateAo12(arrayTimes) {
-    let arrayInDate = [];
+  let arrayInDate = [];
 
-    if (arrayTimes.length < 12) {
-        return "The array needs to have at least 12 records";
-    }
+  if (arrayTimes.length < 12) {
+    return "The array needs to have at least 12 records";
+  }
 
-    arrayTimes.forEach(function (unit) {
-        let formattedTime = formatTimeWithoutMinutes(unit);
-        let timeInDate = timeToDate(formattedTime)
-        arrayInDate.push(timeInDate)
-    });
+  arrayTimes.forEach(function (unit) {
+    let formattedTime = formatTimeWithoutMinutes(unit);
+    let timeInDate = timeToDate(formattedTime);
+    arrayInDate.push(timeInDate);
+  });
 
-    arrayInDate = arrayInDate.slice(-12);
-    arrayInDate.sort(function (a, b) {
-        return a - b;
-    });
-    
-    arrayInDate = arrayInDate.slice(1, -1);
-    var sum = arrayInDate.reduce(function (total, time) {
-        return total + time;
-    }, 0);
+  arrayInDate = arrayInDate.slice(-12);
+  arrayInDate.sort(function (a, b) {
+    return a - b;
+  });
 
-    var average = sum / arrayInDate.length;
+  arrayInDate = arrayInDate.slice(1, -1);
+  var sum = arrayInDate.reduce(function (total, time) {
+    return total + time;
+  }, 0);
 
-    return average;
+  var average = sum / arrayInDate.length;
+
+  return average;
 }
 
-export function totalOfSolves (arrayTimes) {
-    return arrayTimes.length;
+export function totalOfSolves(arrayTimes) {
+  return arrayTimes.length;
 }
 
-export function getBestSolve (arrayTimes) {
-    let arrayInDate = [];
+export function getBestSolve(arrayTimes) {
+  let arrayInDate = [];
 
-    arrayTimes.forEach(function (unit) {
-        let formattedTime = formatTimeWithoutMinutes(unit);
-        let timeInDate = timeToDate(formattedTime)
-        arrayInDate.push(timeInDate)
-    });
+  arrayTimes.forEach(function (unit) {
+    let formattedTime = formatTimeWithoutMinutes(unit);
+    let timeInDate = timeToDate(formattedTime);
+    arrayInDate.push(timeInDate);
+  });
 
-    arrayInDate.sort(function (a, b) {
-        return b - a;
-    });
+  arrayInDate.sort(function (a, b) {
+    return b - a;
+  });
 
-    arrayInDate = arrayInDate.slice(-1);
+  arrayInDate = arrayInDate.slice(-1);
 
-    return arrayInDate[0];
+  return arrayInDate[0];
 }
