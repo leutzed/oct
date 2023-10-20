8b2bfa22f52af074b55c9e97538196cb7b1468b6
Daniel Leutze <leutzed@gmail.com>
2023-10-20 17:57:28 +0000

    dark mode is working

--- src/scripts/darkMode.js
+++ src/scripts/darkMode.js
@@ -1,14 +1,36 @@
 const toggle = document.querySelector(".slider");
-const theme = window.localStorage.getItem("theme");
+let theme = window.localStorage.getItem("theme");
 
+// Verifique se o valor em localStorage está vazio e defina-o como 'light' por padrão
+if (!theme) {
+  theme = "light";
+  window.localStorage.setItem("theme", theme);
+}
+
+function toggleDarkMode() {
+  document.body.classList.toggle("dark-mode");
+  if (theme === "dark") {
+    theme = "light";
+  } else {
+    theme = "dark";
+  }
+  window.localStorage.setItem("theme", theme);
+}
+
+// Atualize a classe com base no valor de 'theme' do localStorage
 if (theme === "dark") {
   document.body.classList.add("dark-mode");
 }
 
 toggle.addEventListener("click", () => {
   console.log('click');
-  document.body.classList.toggle("dark-mode");
+  toggleDarkMode();
+});
+
+// Você também pode adicionar um ouvinte de evento DOMContentLoaded para garantir que a classe
+// seja definida corretamente no carregamento da página
+document.addEventListener("DOMContentLoaded", () => {
   if (theme === "dark") {
-    window.localStorage.setItem("theme", "light");
-  } else window.localStorage.setItem("theme", "dark");
+    document.body.classList.add("dark-mode");
+  }
 });
